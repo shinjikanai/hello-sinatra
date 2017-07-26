c0af8fb66a232f65f69a695108bef062a1af3f6e
shinjikanai <shinjikanai@users.noreply.github.com>
2017-07-26 17:52:19 +1000

    Update foo.rb

--- foo.rb
+++ foo.rb
@@ -2,10 +2,15 @@ require 'rubygems'
 require 'sinatra'
 
 #set :port, ENV['VCAP_APP_PORT']
+set :port, ENV['CF_INSTANCE_PORT']
 
 get '/' do
   host = ENV['VCAP_APP_HOST']
   port = ENV['VCAP_APP_PORT']
+  hostsk = ENV['CF_INSTANCE_IP'] || "192.168.0.0"
+  portsk = ENV['CF_INSTANCE_PORT'] || "9292"
+  indexsk = ENV['CF_INSTANCE_INDEX'] || "0"
+
   msg = "<h1>Hello from VCAP! via: #{host}:#{port}</h1>"
   puts msg
   msg
